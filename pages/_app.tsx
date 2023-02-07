ce7214f3d2a8398c730290d4af4408c48f60dc08
carla <carla@mail.com>
2023-02-07 23:01:35 +0100

    feature: auth0

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import AuthProvider from "@/context/auth";
 import "@/styles/globals.css";
-// import { Auth0Provider } from "@auth0/auth0-react";
+import { Auth0Provider } from "@auth0/auth0-react";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ThemeProvider } from "@emotion/react";
@@ -10,18 +10,18 @@ const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    // <Auth0Provider
-    //   domain="unit-auth-test.jp.auth0.com"
-    //   clientId="sqsj3bbA5p1ricYYxQqCNBAPNBUZHZzZ"
-    //   authorizationParams={{ redirect_uri: "http://localhost:3000" }}
-    // >
-    <ThemeProvider theme={AppTheme}>
-      <QueryClientProvider client={queryClient}>
-        <AuthProvider>
-          <Component {...pageProps} />
-        </AuthProvider>
-      </QueryClientProvider>
-    </ThemeProvider>
-    // </Auth0Provider>
+    <Auth0Provider
+      domain="unit-auth-test.jp.auth0.com"
+      clientId="sqsj3bbA5p1ricYYxQqCNBAPNBUZHZzZ"
+      authorizationParams={{ redirect_uri: "http://localhost:3000", audience: "https://localhost:8000" }}
+    >
+      <ThemeProvider theme={AppTheme}>
+        <QueryClientProvider client={queryClient}>
+          <AuthProvider>
+            <Component {...pageProps} />
+          </AuthProvider>
+        </QueryClientProvider>
+      </ThemeProvider>
+    </Auth0Provider>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import LoadingButton from "@/component/general/loadingButton";
 import CustomSnackBar from "@/component/general/snackbar";
 import { useAuthContex } from "@/context/auth";
 import { ISignInData } from "@/interface/auth";
+import { useAuth0 } from "@auth0/auth0-react";
 import { Button, Card, CardContent, Container, Stack, TextField, Typography } from "@mui/material";
 import Link from "next/link";
 import { useRouter } from "next/router";
@@ -12,6 +13,21 @@ export default function AuthPage() {
   const { login } = useAuthContex();
   const [snackbar, setSnackbar] = useState<{ error?: string; success?: string }>({});
   const [loading, setLoading] = useState(false);
+  const auth0 = useAuth0();
+
+  console.log(auth0);
+
+  async function getData() {
+    const func = await auth0.getAccessTokenSilently({
+      // authorizationParams: { audience: "https://localhost:8000" },
+    });
+    const token = await auth0.getIdTokenClaims();
+    console.log({ func, token });
+  }
+
+  function loginS() {
+    auth0.loginWithRedirect();
+  }
 
   const handleLogin = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -34,13 +50,17 @@ export default function AuthPage() {
   };
   return (
     <Stack justifyContent={"center"} alignItems="center" sx={{ minHeight: "100vh" }}>
+      <Button onClick={getData}> Click </Button>
+      <Button onClick={loginS}> Login </Button>
       <Card elevation={0}>
         <Stack p={"1em"} gap={"1em"} justifyContent="center">
-          <Typography> Login to Continue or <Link href={"/signup"}>
-           <Button> Sign Up </Button>
-          
-          </Link>
-            </Typography>
+          <Typography>
+            {" "}
+            Login to Continue or{" "}
+            <Link href={"/signup"}>
+              <Button> Sign Up </Button>
+            </Link>
+          </Typography>
           <form onSubmit={handleLogin}>
             <Stack gap={"2em"}>
               <TextField label="Email" name="email" type={"email"} />
